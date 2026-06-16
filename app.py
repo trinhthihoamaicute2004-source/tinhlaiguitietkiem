5ffb54eae4bb170deeee72547fb2f43d070c47f3
trinhthihoamaicute2004-source <trinhthihoamaicute2004@gmail.com>
2026-06-16 14:02:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰App tính tiền gửi tiết kiệm_đề tài 6_ Trịnh Thị Hoa Mai")
 
 # Nhập dữ liệu
 C = st.number_input(
